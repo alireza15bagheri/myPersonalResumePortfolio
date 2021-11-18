aebd72a20bdc0affe2d8868505baabe003cb23e6
alireza bagheri <alireza15.bagheri@gmail.com>
2021-11-18 17:34:22 +0330

    Added 3D effect to the portfolio Cards

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
+const containers = document.querySelectorAll('.portfolio .box-container .box');
+const card = document.querySelectorAll('.card');
+const { width, height } = containers[1].getBoundingClientRect();
+
+for (let item of containers.entries()) {
+
+    item[1].addEventListener('mousemove', (event) => {
+        const { offsetX, offsetY } = event;
+
+        card[item[0]].style.setProperty('--x-pos', (offsetX / width) - 0.5);
+        card[item[0]].style.setProperty('--y-pos', (offsetY / width) - 0.5);
+    })
+}
+
+
+
 $(document).ready(function () {
+
 
     $('#menu').click(function () {
         $(this).toggleClass('fa-times');
